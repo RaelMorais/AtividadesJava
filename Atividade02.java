fdce21878c494ef608759ab3da555f23de6fc76d
Israel Santana <125073383+RaelMorais@users.noreply.github.com>
2025-02-05 16:55:47 -0300

    Add files via upload

---
+++ Atividade02.java
@@ -0,0 +1,39 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Atividade02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantas pessoas deseja adicionar?");
+        int idadeAcima50 =0;
+        int alturaAcima160 =0;
+        int pesoAbaixo80 =0;
+        int pessoas = scanner.nextInt();
+        for (int i = 0; i < pessoas; i++){
+            System.out.println("Pessoa " + i);
+
+            System.out.println("Idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Peso");
+            double peso = scanner.nextDouble();
+
+            System.out.print("Digite a altura (em metros, ex: 1.75): ");
+            double altura = scanner.nextDouble();
+
+            if(idade > 50){
+                idadeAcima50++;
+            }
+            if(altura > 1.60){
+                alturaAcima160++;
+            }
+            if(peso < 80){
+                pesoAbaixo80++;
+            }
+        }
+        System.out.println("Qtd de pessoas com idade acima de 50:" +idadeAcima50);
+        System.out.println("Qtd de pessoas com altura acima de 1.60:" +alturaAcima160);
+        System.out.println("Qtd de pessoas com peso abaixo de 80kg:" +pesoAbaixo80);
+    }
+}
